9ceae3bedfbf70960fa1f4e5c6efd830f3728020
LongYinan <lynweklm@gmail.com>
2025-10-30 15:05:26 +0800

    feat(cli): add sync remote subcommand (#278)

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,17 +1,20 @@
-import { replaceFileContent } from './replace-file-content';
-import { snapTest } from './snap-test';
-
 const subcommand = process.argv[2];
 
 switch (subcommand) {
   case 'snap-test':
+    const { snapTest } = await import('./snap-test');
     await snapTest();
     break;
   case 'replace-file-content':
+    const { replaceFileContent } = await import('./replace-file-content');
     replaceFileContent();
+    break;
+  case 'sync-remote':
+    const { syncRemote } = await import('./sync-remote-deps');
+    syncRemote();
     break;
   default:
     console.error(`Unknown subcommand: ${subcommand}`);
-    console.error('Available subcommands: snap-test, replace-file-content');
+    console.error('Available subcommands: snap-test, replace-file-content, sync-remote');
     process.exit(1);
 }
